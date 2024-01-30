3350f83d9d6541fefeb2ae3a548a0bb50bc00ea1
Ahmed BOUCHFAA <abouchfaa@gmail.com>
2024-01-30 10:51:47 +0100

    Initial Commit

---
+++ RijksBrowser/RijksBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  RijksBrowserApp.swift
+//  RijksBrowser
+//
+//  Created by Ahmed Bouchfaa on 30/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct RijksBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
